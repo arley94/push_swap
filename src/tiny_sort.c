edbff2dba73d0fdccdbe7dda9062022d48f97964
acoto-gu <arleycoto94@gmail.com>
2024-02-02 08:38:41 +0100

    Add function to sort 3 elements

---
+++ src/tiny_sort.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tiny_sort.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 16:52:22 by acoto-gu          #+#    #+#             */
+/*   Updated: 2024/02/02 08:37:38 by acoto-gu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	tiny_sort(t_stack_node **a)
+{
+	if (((*a)->nb > (*a)->next->nb) && ((*a)->nb > (*a)->next->next->nb))
+		ra(a);
+	else if (((*a)->next->nb > (*a)->nb)
+		&& ((*a)->next->nb > (*a)->next->next->nb))
+		rra(a);
+	if ((*a)->nb > (*a)->next->nb)
+		sa(a);
+}

acca8a13c045509e2868a7f07331722eb9dcd587
acoto-gu <arleycoto94@gmail.com>
2024-02-04 12:48:20 +0100

    Add get_stack_max_min function to get the biggest or smallest node in the stack

--- src/stack_utils.c
+++ src/stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 08:28:14 by acoto-gu          #+#    #+#             */
-/*   Updated: 2024/02/01 16:46:49 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2024/02/04 12:39:56 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,4 +64,27 @@ int	ft_is_stack_sorted(t_stack_node *stack)
 		stack = stack->next;
 	}
 	return (1);
+}
+
+t_stack_node	*get_stack_max_min(t_stack_node *stack,
+					t_node_limit_value max_min)
+{
+	t_stack_node	*biggest_or_smallest;
+
+	biggest_or_smallest = stack;
+	while (stack)
+	{
+		if (max_min == BIGGEST_NODE)
+		{
+			if (biggest_or_smallest->nb < stack->nb)
+				biggest_or_smallest = stack;
+		}
+		else
+		{
+			if (biggest_or_smallest->nb > stack->nb)
+				biggest_or_smallest = stack;
+		}
+		stack = stack->next;
+	}
+	return (biggest_or_smallest);
 }

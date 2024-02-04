2934e26164d51d057bfd1e60b24df5665679e58c
acoto-gu <arleycoto94@gmail.com>
2024-02-04 15:43:16 +0100

    Organize header file by sections

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:37:48 by acoto-gu          #+#    #+#             */
-/*   Updated: 2024/02/04 12:43:17 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2024/02/04 14:35:24 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include "../libs/libftprintf/ft_printf.h"
 
 /* ************************************************************************** */
-/*                                 STRUCT                                     */
+/*                                 DATA_TYPES                                 */
 /* ************************************************************************** */
 
 typedef enum e_stack_pos
@@ -51,25 +51,53 @@ typedef struct s_stack_node
 }					t_stack_node;
 
 /* ************************************************************************** */
-/*                                   STACK                                    */
+/*                                 PARSE-ARGS                                 */
+/* ************************************************************************** */
+
+void			ft_parse_args(char **argv, t_stack_node **stack_a);
+int				ft_atoi_special(char *str_nb, int *nb);
+
+/* ************************************************************************** */
+/*                                ERRORS-FREE                                 */
+/* ************************************************************************** */
+
+void			ft_free_str_arr(char **str_arr);
+void			ft_stack_clear(t_stack_node **stack);
+int				ft_check_dup(t_stack_node *stack_a, int nb);
+
+/* ************************************************************************** */
+/*                                STACK UTILS                                 */
 /* ************************************************************************** */
 
 t_stack_node	*ft_last_node(t_stack_node *stack);
 void			ft_add_node_back(t_stack_node **stack, t_stack_node *new);
-
-/* ************************************************************************** */
-/*                                   PARSE                                    */
-/* ************************************************************************** */
-
-// void	ft_add_numb_to_stack(int numb, t_stack **stack_a);
-// void	ft_save_numbs(char **split_numbs, t_stack **stack_a);
-// //void	ft_free_str_arr(char **str_arr);
-void			ft_parse_args(char **argv, t_stack_node **stack_a);
-int				ft_atoi_special(char *str_nb, int *nb);
-void			ft_free_str_arr(char **str_arr);
-void			ft_stack_clear(t_stack_node **stack);
-int				ft_check_dup(t_stack_node *stack_a, int nb);
 int				ft_stack_len(t_stack_node *stack);
+int				ft_is_stack_sorted(t_stack_node *stack);
+t_stack_node	*get_stack_max_min(t_stack_node *stack,
+					t_node_limit_value max_min);
+
+/* ************************************************************************** */
+/*                                ALGORITHMS                                  */
+/* ************************************************************************** */
+
+void			small_sort(t_stack_node **a);
+void			big_sort(t_stack_node **a, t_stack_node **b);
+
+/* ************************************************************************** */
+/*                               BIG_SORT_UTILS                               */
+/* ************************************************************************** */
+
+void			set_index_and_half(t_stack_node *stack);
+void			set_targets_to_a(t_stack_node *a, t_stack_node *b);
+void			set_targets_to_b(t_stack_node *b, t_stack_node *a);
+void			set_prices(t_stack_node *a, t_stack_node *b);
+void			init_stacks(t_stack_node *dst, t_stack_node *src,
+					t_stack_nb dst_stack);
+
+/* ************************************************************************** */
+/*                                   COMMANDS                                 */
+/* ************************************************************************** */
+
 void			sa(t_stack_node **a);
 void			sb(t_stack_node **b);
 void			ss(t_stack_node **a, t_stack_node **b);
@@ -81,16 +109,5 @@ void			rr(t_stack_node **a, t_stack_node **b);
 void			rra(t_stack_node **a);
 void			rrb(t_stack_node **b);
 void			rrr(t_stack_node **a, t_stack_node **b);
-int				ft_is_stack_sorted(t_stack_node *stack);
-void			small_sort(t_stack_node **a);
-void			set_index_and_half(t_stack_node *stack);
-t_stack_node	*get_stack_max_min(t_stack_node *stack,
-					t_node_limit_value max_min);
-void			set_targets_to_a(t_stack_node *a, t_stack_node *b);
-void			set_targets_to_b(t_stack_node *b, t_stack_node *a);
-void			set_prices(t_stack_node *a, t_stack_node *b);
-void			init_stacks(t_stack_node *dst, t_stack_node *src,
-					t_stack_nb dst_stack);
-void			big_sort(t_stack_node **a, t_stack_node **b);
 
 #endif

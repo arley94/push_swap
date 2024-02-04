336f625764868383ed76a37b1aed64817a9c2b16
acoto-gu <arleycoto94@gmail.com>
2024-02-04 09:03:28 +0100

    update main

--- src/main.c
+++ src/main.c
@@ -6,33 +6,44 @@
 /*   By: acoto-gu <acoto-gu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:26:49 by acoto-gu          #+#    #+#             */
-/*   Updated: 2024/01/30 11:20:09 by acoto-gu         ###   ########.fr       */
+/*   Updated: 2024/02/04 09:06:01 by acoto-gu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	ft_print_int(void *nb)
+void	ft_print_stack(t_stack_node *a)
 {
-	ft_printf("%d\n", *((int *)nb));
+	while (a)
+	{
+		ft_printf("%d\n", a->nb);
+		a = a->next;
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	t_stack_node	*stack_a;
-	t_stack_node	*temp_a;
+	t_stack_node	*a;
+	t_stack_node	*b;
 
-	stack_a = NULL;
-	if (argc > 1)
+	a = NULL;
+	b = NULL;
+	if (argc == 1)
+		return (1);
+	ft_parse_args(argv + 1, &a);
+	// ft_printf("Stack a before sort:\n");
+	// ft_print_stack(a);
+	if (!ft_is_stack_sorted(a))
 	{
-		ft_parse_args(argv + 1, &stack_a);
-		temp_a = stack_a;
-		while (temp_a)
-		{
-			ft_printf("%d\n", temp_a->nb);
-			temp_a = temp_a->next;
-		}
-		ft_stack_clear(&stack_a);
+		if (ft_stack_len(a) == 2)
+			sa(&a);
+		else if (ft_stack_len(a) == 3)
+			small_sort(&a);
+		else
+			big_sort(&a, &b);
 	}
+	// ft_printf("Stack a after sort:\n");
+	// ft_print_stack(a);
+	ft_stack_clear(&a);
 	return (0);
 }
